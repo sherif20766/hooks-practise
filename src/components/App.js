461928181b52b521034195d1a9ab4d47b6a8c635
sherif20766 <sherif20766@hotmail.com>
2020-01-03 17:40:09 +0000

    configured the App functional component to render out the current state depending on button pressed, using useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-	return <div>App</div>;
+	const [resource, setResource] = useState("posts");
+
+	return (
+		<div>
+			<div>
+				<button
+					className="ui button primary"
+					onClick={() => setResource("posts")}
+				>
+					Posts
+				</button>
+				<button
+					className="ui button secondary"
+					onClick={() => setResource("todos")}
+				>
+					Todos
+				</button>
+			</div>
+			{resource}
+		</div>
+	);
 };
 
 export default App;
