68e75bef0a2565cf976b3e69f5ae82b95eb9fe86
sherif20766 <sherif20766@hotmail.com>
2020-01-04 13:47:06 +0000

    extracted hooks logic from component for reuseability

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
 	const [resources, setResources] = useState([]);
 
 	const fetchResource = async resource => {
@@ -15,6 +15,11 @@ const ResourceList = ({ resource }) => {
 		fetchResource(resource);
 	}, [resource]);
 
+	return resources;
+};
+
+const ResourceList = ({ resource }) => {
+	const resources = useResources(resource);
 	return (
 		<ul>
 			{resources.map(record => {
