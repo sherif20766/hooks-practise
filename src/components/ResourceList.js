f4a16a8165f3659714b2de47054f32830930fab3
sherif20766 <sherif20766@hotmail.com>
2020-01-04 13:59:58 +0000

    extracted the useResources function into a seperate re-usable file and imported it into the ResourceList component

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,22 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const useResources = resource => {
-	const [resources, setResources] = useState([]);
-
-	const fetchResource = async resource => {
-		const response = await axios.get(
-			`https://jsonplaceholder.typicode.com/${resource}`
-		);
-		setResources(response.data);
-	};
-
-	useEffect(() => {
-		fetchResource(resource);
-	}, [resource]);
-
-	return resources;
-};
+import React from "react";
+import useResources from "./useResources";
 
 const ResourceList = ({ resource }) => {
 	const resources = useResources(resource);
